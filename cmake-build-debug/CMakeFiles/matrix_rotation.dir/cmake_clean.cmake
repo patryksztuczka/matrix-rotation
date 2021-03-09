b86c2a1cdbb846a6c1c19da70b4f6e4ddf51ca19
Patryk Sztuczka <73931963+patryksztuczka@users.noreply.github.com>
2021-03-09 14:57:14 +0100

    ignore files

---
+++ cmake-build-debug/CMakeFiles/matrix_rotation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_rotation.dir/main.cpp.obj"
+  "libmatrix_rotation.dll.a"
+  "matrix_rotation.exe"
+  "matrix_rotation.exe.manifest"
+  "matrix_rotation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_rotation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
