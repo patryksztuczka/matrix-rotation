adf5cb4a10b3672e87c69953e48459cf5e2cdca6
Patryk Sztuczka <73931963+patryksztuczka@users.noreply.github.com>
2021-03-09 15:16:00 +0100

    rotation of 2x2 matrix

--- main.cpp
+++ main.cpp
@@ -5,24 +5,40 @@ using namespace std;
 const int MATRIX_SIZE = 2;
 
 void printMatrix(int m[MATRIX_SIZE][MATRIX_SIZE]){
-    cout << "========== Orginal matrix =========" << endl;
     for (int i = 0; i < MATRIX_SIZE; i++) {
         for (int j = 0; j < MATRIX_SIZE; j++) {
             cout << m[i][j] << "   ";
         }
         cout << endl;
     }
+    cout << "==============================" << endl;
+}
+
+void swap(int &i, int &j){
+    int temp = i;
+    i = j;
+    j = temp;
 }
 
 
 int main() {
     int m[MATRIX_SIZE][MATRIX_SIZE];
+    int last = MATRIX_SIZE - 1;
 
     for (int i = 0; i < MATRIX_SIZE; i++) {
         for (int j = 0; j < MATRIX_SIZE; j++) {
             cout << "Enter an element of matrix [" << i << "," << j << "]: " << endl;
             cin >> m[i][j];
         }
+    }
+
+    cout << "========== Orginal matrix =========" << endl;
+    printMatrix(m);
+
+    for (int i = 0; i < last; i++) {
+        swap(m[i][i], m[i][last]);
+        swap(m[i][i], m[last][last]);
+        swap(m[i][i], m[last][i]);
     }
 
     printMatrix(m);
